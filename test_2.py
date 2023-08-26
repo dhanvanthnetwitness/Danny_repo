22da45b9c616a80b15c8d8bdef08c24b6faa7eaf
Dhanvanth <dhanvanth.a@netwitness.com>
2023-08-26 12:23:31 +0530

    Create test_2.py
    shortest code

---
+++ test_2.py
@@ -0,0 +1,10 @@
+arr= ["carracelightcar","carracecarlightcar","carracelightcar"]
+#arr =  sorted(arr,key=len) easy 
+lent=min(len(arr[0]),len(arr[1]),len(arr[2]))
+#for i in range(0,len(arr[0])):
+for i in range(0,lent):
+    if arr[0][i]!=arr[1][i] or arr[1][i]!=arr[2][i] or arr[0][i]!=arr[2][i]:
+        print(arr[0][:i])
+        break
+
+
